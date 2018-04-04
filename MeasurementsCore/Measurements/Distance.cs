b57cfb8209f836bea6cd774ccc3e176badb83f19
Justus Bonz <jasdefer>
2018-04-04 16:49:24 +0200

    Start adding some basic measurements

---
+++ MeasurementsCore/Measurements/Distance.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace Measurements
+{
+    public struct Distance
+    {
+        /// <summary>
+        /// Ensures, that the meter representation in int is still valid
+        /// </summary>
+        public const long MaxMillimeters = (long)MillimetersPerMeter * int.MaxValue;
+        public const long MinMillimeters = 0;
+
+        public const int MillimetersPerCentimeter = 10;
+        public const int MillimetersPerMeter = 1000;
+        public const int MillimetersPerKiloMeter = 1000000;
+
+        public static Distance MinValue = new Distance(MinMillimeters);
+        public static Distance MaxValue = new Distance(MaxMillimeters);
+
+        public long Millimeters { get; }
+        public long Centimeters { get => Millimeters / MillimetersPerCentimeter; }
+        public int Meters { get => (int)(Millimeters / MillimetersPerMeter); }
+        public int Kilometers { get => (int)Millimeters / MillimetersPerKiloMeter; }
+
+        public Distance(long millimeters)
+        {
+            if (millimeters < 0 || millimeters > MaxMillimeters)
+            {
+                throw new ArgumentOutOfRangeException("Invalid Distance");
+            }
+            Millimeters = millimeters;
+        }
+
+        public Distance(int meters) : this(MillimetersPerMeter * (long)meters) { }
+
+        public static Distance FromMillimeters(long millimeters)
+        {
+            return new Distance(millimeters);
+        }
+
+        public override string ToString()
+        {
+            return $"{Meters}m";
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Distance)) return false;
+            return this == (Distance)obj;
+        }
+
+        public static bool operator ==(Distance distance1, Distance distance2)
+        {
+            return distance1.Millimeters == distance2.Millimeters;
+        }
+
+        public static bool operator !=(Distance distance1, Distance distance2)
+        {
+            return distance1.Millimeters != distance2.Millimeters;
+        }
+
+        public override int GetHashCode()
+        {
+            return Millimeters.GetHashCode();
+        }
+
+        public static bool operator <(Distance distanceOne, Distance distanceTwo)
+        {
+            return distanceOne.Millimeters < distanceTwo.Millimeters;
+        }
+
+        public static bool operator >(Distance distanceOne, Distance distanceTwo)
+        {
+            return distanceOne.Millimeters > distanceTwo.Millimeters;
+        }
+
+        public static bool operator <=(Distance distanceOne, Distance distanceTwo)
+        {
+            return distanceOne.Millimeters <= distanceTwo.Millimeters;
+        }
+
+        public static bool operator >=(Distance distanceOne, Distance distanceTwo)
+        {
+            return distanceOne.Millimeters >= distanceTwo.Millimeters;
+        }
+
+        public static Distance operator +(Distance distanceOne, Distance distanceTwo)
+        {
+            return FromMillimeters(distanceOne.Millimeters + distanceTwo.Millimeters);
+        }
+
+        public static Distance operator *(Distance distance, int multiplier)
+        {
+            return FromMillimeters(distance.Millimeters * multiplier);
+        }
+    }
+}
---
+++ MeasurementsCore/Measurements/Velocity.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace Measurements
+{
+    public class Velocity
+    {
+        /// <summary>
+        /// Distance per second
+        /// </summary>
+        private readonly Distance Distance;
+
+        /// <summary>
+        /// Initializes a new instance of the Velocity structure to the specified distance per second.
+        /// </summary>
+        public Velocity(Distance distance)
+        {
+            Distance = distance;
+        }
+
+        public Velocity(int metersPerSecond) : this(new Distance(metersPerSecond)) { }
+
+        public Velocity(Distance distance, TimeSpan deltaTime)
+        {
+            Distance = new Distance(distance.Millimeters/(deltaTime.Ticks*TimeSpan.TicksPerSecond));
+        }
+
+        public TimeSpan GetDuration(Distance distance)
+        {
+            if (distance.Millimeters == 0) return new TimeSpan();
+            return TimeSpan.FromTicks(TimeSpan.TicksPerSecond * distance.Meters / Distance.Meters);
+        }
+
+        public static TimeSpan operator /(Distance distance, Velocity velocity)
+        {
+            return TimeSpan.FromTicks(TimeSpan.TicksPerSecond * distance.Meters / velocity.Distance.Meters);
+        }
+
+        public static bool operator <(Velocity v1, Velocity v2)
+        {
+            return v1.Distance.Millimeters < v2.Distance.Millimeters;
+        }
+
+        public static bool operator <=(Velocity v1, Velocity v2)
+        {
+            return v1.Distance.Millimeters <= v2.Distance.Millimeters;
+        }
+
+        public static bool operator >(Velocity v1, Velocity v2)
+        {
+            return v1.Distance.Millimeters > v2.Distance.Millimeters;
+        }
+
+        public static bool operator >=(Velocity v1, Velocity v2)
+        {
+            return v1.Distance.Millimeters >= v2.Distance.Millimeters;
+        }
+
+        public static bool operator ==(Velocity v1, Velocity v2)
+        {
+            return v1.Distance.Millimeters == v2.Distance.Millimeters;
+        }
+
+        public static bool operator !=(Velocity v1, Velocity v2)
+        {
+            return v1.Distance.Millimeters != v2.Distance.Millimeters;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Velocity)) return false;
+            return Distance.Millimeters == ((Distance)obj).Millimeters;
+        }
+
+        public override int GetHashCode()
+        {
+            return Distance.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"{Distance.Meters}m/s";
+        }
+    }
+}
