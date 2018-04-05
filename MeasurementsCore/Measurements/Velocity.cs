1d68636bda99f2cf69943c3f27ae131a65360dc0
Justus Bonz <jasdefer>
2018-04-05 09:56:02 +0200

    Bug fixes and added unit tests for the velocity struct

--- MeasurementsCore/Measurements/Velocity.cs
+++ MeasurementsCore/Measurements/Velocity.cs
@@ -4,17 +4,21 @@ namespace Measurements
 {
     public class Velocity
     {
+        public const int MaxMetersPerSecond = 299792458;
+        public static readonly Velocity SpeedOfLight = new Velocity(MaxMetersPerSecond);
+
         /// <summary>
         /// Distance per second
         /// </summary>
-        private readonly Distance Distance;
+        private readonly Distance distance;
 
         /// <summary>
         /// Initializes a new instance of the velocity structure to the specified distance per second.
         /// </summary>
         public Velocity(Distance distance)
         {
-            Distance = distance;
+            if (distance.Meters > MaxMetersPerSecond) throw new ArgumentOutOfRangeException(nameof(distance));
+            this.distance = distance;
         }
 
         /// <summary>
@@ -30,8 +34,20 @@ namespace Measurements
         /// <param name="deltaTime">The timespam part of the velocity.</param>
         public Velocity(Distance distance, TimeSpan deltaTime)
         {
-            Distance = new Distance(distance.Millimeters/(deltaTime.Ticks*TimeSpan.TicksPerSecond));
+            //Ensure that the distances calculation does not exceed the long.MaxValue limit
+            if (distance.Millimeters > long.MaxValue / TimeSpan.TicksPerSecond)
+            {
+                throw new ArgumentOutOfRangeException(nameof(distance),"The distance is to large. Consider decreasing the distance as well as the delta time.");
+            }
+
+            this.distance = new Distance(TimeSpan.TicksPerSecond*distance.Millimeters/ deltaTime.Ticks );
+            if (this > SpeedOfLight) throw new ArgumentOutOfRangeException(nameof(distance));
         }
+
+        /// <summary>
+        /// Get the velocity in meters per second.
+        /// </summary>
+        public int MetersPerSecond => distance.Meters;
 
         /// <summary>
         /// Get the timespan needed to cover the specified distance with this velocity.
@@ -39,58 +55,65 @@ namespace Measurements
         public TimeSpan GetDuration(Distance distance)
         {
             if (distance.Millimeters == 0) return new TimeSpan();
-            return TimeSpan.FromTicks(TimeSpan.TicksPerSecond * distance.Meters / Distance.Meters);
+            else if (this.distance.Millimeters == 0) throw new Exception("A velocity of 0 m/s cannot cover any distances greater than 0.");
+
+            //Ensure that the duration calculation does not exceed the long.MaxValue limit
+            if (distance.Millimeters> long.MaxValue / TimeSpan.TicksPerSecond)
+            {
+                throw new ArgumentOutOfRangeException(nameof(distance));
+            }
+            return TimeSpan.FromTicks(TimeSpan.TicksPerSecond * distance.Millimeters / this.distance.Millimeters);
         }
 
         public static TimeSpan operator /(Distance distance, Velocity velocity)
         {
-            return TimeSpan.FromTicks(TimeSpan.TicksPerSecond * distance.Meters / velocity.Distance.Meters);
+            return TimeSpan.FromTicks(TimeSpan.TicksPerSecond * distance.Meters / velocity.distance.Meters);
         }
 
         public static bool operator <(Velocity v1, Velocity v2)
         {
-            return v1.Distance.Millimeters < v2.Distance.Millimeters;
+            return v1.distance.Millimeters < v2.distance.Millimeters;
         }
 
         public static bool operator <=(Velocity v1, Velocity v2)
         {
-            return v1.Distance.Millimeters <= v2.Distance.Millimeters;
+            return v1.distance.Millimeters <= v2.distance.Millimeters;
         }
 
         public static bool operator >(Velocity v1, Velocity v2)
         {
-            return v1.Distance.Millimeters > v2.Distance.Millimeters;
+            return v1.distance.Millimeters > v2.distance.Millimeters;
         }
 
         public static bool operator >=(Velocity v1, Velocity v2)
         {
-            return v1.Distance.Millimeters >= v2.Distance.Millimeters;
+            return v1.distance.Millimeters >= v2.distance.Millimeters;
         }
 
         public static bool operator ==(Velocity v1, Velocity v2)
         {
-            return v1.Distance.Millimeters == v2.Distance.Millimeters;
+            return v1.distance.Millimeters == v2.distance.Millimeters;
         }
 
         public static bool operator !=(Velocity v1, Velocity v2)
         {
-            return v1.Distance.Millimeters != v2.Distance.Millimeters;
+            return v1.distance.Millimeters != v2.distance.Millimeters;
         }
 
         public override bool Equals(object obj)
         {
             if (!(obj is Velocity)) return false;
-            return Distance.Millimeters == ((Distance)obj).Millimeters;
+            return distance.Millimeters == ((Distance)obj).Millimeters;
         }
 
         public override int GetHashCode()
         {
-            return Distance.GetHashCode();
+            return distance.GetHashCode();
         }
 
         public override string ToString()
         {
-            return $"{Distance.Meters}m/s";
+            return $"{distance.Meters}m/s";
         }
     }
 }
--- MeasurementsCore/MeasurementsUnitTests/VelocityUnitTests.cs
+++ MeasurementsCore/MeasurementsUnitTests/VelocityUnitTests.cs
@@ -1,13 +1,80 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Measurements;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace MeasurementsUnitTests
 {
+    [TestClass]
     public class VelocityUnitTests
     {
-        
+        [DataTestMethod]
+        [DataRow(0)]
+        [DataRow(1)]
+        [DataRow(Velocity.MaxMetersPerSecond)]
+        public void InitializeByMeterPerSecond(int metersPerSecond)
+        {
+            Velocity velocity = new Velocity(metersPerSecond);
+            Assert.AreEqual(metersPerSecond, velocity.MetersPerSecond);
+        }
+
+        [DataTestMethod]
+        [DataRow(0)]
+        [DataRow(1)]
+        [DataRow(Velocity.MaxMetersPerSecond)]
+        public void InitializeByDistance(int meters)
+        {
+            Distance distance = new Distance(meters);
+            Velocity velocity = new Velocity(distance);
+            Assert.AreEqual(meters, velocity.MetersPerSecond);
+        }
+
+        [DataTestMethod]
+        [DataRow(0)]
+        [DataRow(1)]
+        [DataRow(2)]
+        [DataRow(Velocity.MaxMetersPerSecond)]
+        public void InitializeByDistanceAndTime(int meters)
+        {
+            int seconds = 2;
+            Distance distance = new Distance(meters);
+            TimeSpan deltaTime = TimeSpan.FromSeconds(seconds);
+            Velocity velocity = new Velocity(distance, deltaTime);
+            Assert.AreEqual(meters/ seconds, velocity.MetersPerSecond);
+        }
+
+        [TestMethod]
+        public void InitializeMaxByDistanceAndTime()
+        {
+            long millimeters = long.MaxValue / TimeSpan.TicksPerSecond;
+            Distance distance = new Distance(millimeters);
+
+            //Ensure that the speed of light is not exceeded
+            TimeSpan deltaTime = TimeSpan.FromSeconds(millimeters/1000);
+
+            Velocity velocity = new Velocity(distance, deltaTime);
+            Assert.AreEqual(1, velocity.MetersPerSecond);
+        }
+
+        [TestMethod]
+        public void TooLargeVelocityByDistance()
+        {
+            Distance distance = new Distance(Velocity.MaxMetersPerSecond + 1);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => new Velocity(distance));
+        }
+
+        [TestMethod]
+        public void TooLargeVelocityByMetersPerSecond()
+        {
+            int metersPerSecond = Velocity.MaxMetersPerSecond + 1;
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => new Velocity(metersPerSecond));
+        }
+
+        [TestMethod]
+        public void TooLargeVelocityDistanceAndVelocity()
+        {
+            Distance distance = new Distance((long)Velocity.MaxMetersPerSecond * Distance.MillimetersPerMeter + 1);
+            TimeSpan deltaTime = TimeSpan.FromSeconds(1);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => new Velocity(distance, deltaTime));
+        }
     }
 }
